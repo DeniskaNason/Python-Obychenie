62012b2e01bf0bf7d79975838f066fdd51998669
DeniskaNason <125904853+DeniskaNason@users.noreply.github.com>
2023-02-20 19:43:06 +0300

    Delete Алгоритмический тренажер 5.py

--- Алгоритмический тренажер 5.py
+++
@@ -1,6 +0,0 @@
-amount1 = int(input("Введи amount1 "))
-amount2 = int(input("Введи amount2 "))
-if (amount1 > 10 and amount2 < 100) and (amount2 > amount1):
-    print (amount2)
-else:
-    print(amount1)
