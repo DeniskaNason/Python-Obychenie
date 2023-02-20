295cd6ebe8c8b1305a7bda53753098fc17202f07
DeniskaNason <125904853+DeniskaNason@users.noreply.github.com>
2023-02-20 19:45:33 +0300

    Add files via upload

---
+++ Алгоритмический тренажер 1.py
@@ -0,0 +1,5 @@
+product = 0
+while product < 100:
+    a = int(input('Введите число: '))
+    product = a * 10
+    print(product)
---
+++ Алгоритмический тренажер 4.py
@@ -0,0 +1,5 @@
+total = 0
+for a in range(10):
+    a = int(input("Введите число: "))
+    total = total + a
+print(total)
