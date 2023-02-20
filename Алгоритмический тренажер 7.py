c059d15793c2b93b318ed33f834c948f30348f03
DeniskaNason <125904853+DeniskaNason@users.noreply.github.com>
2023-02-20 19:34:31 +0300

    Add files via upload

---
+++ Алгоритмический тренажер 7.py
@@ -0,0 +1,4 @@
+for a in range(10):
+    for b in range(16):
+        print('#', end='')
+    print()
---
+++ Упражнения по программированию 1.py
@@ -0,0 +1,5 @@
+total = 0
+for a in range(5):
+    error = int(input('Сколько ошибок за сегодня? '))
+    total += error
+print('Всего ошибок за 5 дней:', total)
