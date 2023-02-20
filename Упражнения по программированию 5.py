b5b3b8a129c84be97cff12d81693214c829ed73f
DeniskaNason <125904853+DeniskaNason@users.noreply.github.com>
2023-02-20 19:44:55 +0300

    Delete Упражнения по программированию 5.py

--- Упражнения по программированию 5.py
+++
@@ -1,8 +0,0 @@
-a = float(input("Введите массу тела в кг: "))
-weight = a * 9.8
-if weight >= 500:
-    print("Тело слишком тяжелое")
-elif weight <= 100:
-    print("Тело слишком легкое")
-else:
-    print(f'{weight:.2f}')
